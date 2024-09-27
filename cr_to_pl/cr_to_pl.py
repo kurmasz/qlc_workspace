d0e57f459355afd3e376d5751cb2201ca47661cd
Zachary Kurmas <kurmasz@gvsu.edu>
2024-09-27 12:40:31 -0400

    Initial commit

---
+++ cr_to_pl/cr_to_pl.py
@@ -0,0 +1,12 @@
+##############################################################################
+#
+# cr_to_pl.py
+#
+# A script to take a code-review.csv file as input and produce a 
+# PrairieLearn questions for each comment.
+#  
+##############################################################################
+
+
+# The only thing you need to change in info.json for now is the uuid.
+# 
